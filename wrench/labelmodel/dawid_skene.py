b0d6dc738c8d53128dd9ae7757fe559c535f3fd1
Steven An <sla001@eng.ucsd.edu>
2024-01-26 19:33:08 -0800

    allow for different DS models and oracle computation

--- wrench/labelmodel/dawid_skene.py
+++ wrench/labelmodel/dawid_skene.py
@@ -18,11 +18,10 @@ ABSTAIN = -1
 #@njit(parallel=True, nogil=True)
 def help_e_step(Y_p, error_rates, L_aug):
     n, n_class = Y_p.shape
-    for i in prange(n):
+    #for i in prange(n):
+    for i in range(n):
         for j in range(n_class):
-            #Y_p[i, j] *= np.prod(np.power(error_rates[:, j, :], L_aug[i, :, :]))
-            Y_p[i, j] += np.sum(np.log(error_rates[:, j, :]) * L_aug[i, :, :])
-
+            Y_p[i, j] *= np.prod(np.power(error_rates[:, j, :], L_aug[i, :, :]))
 
 #@njit(parallel=True, nogil=True)
 def initialize_Y_p(Y_p, L, n_class):
@@ -55,6 +54,7 @@ class DawidSkene(BaseLabelModel):
             y_valid: Optional[np.ndarray] = None,
             n_class: Optional[int] = None,
             balance: Optional[np.ndarray] = None,
+            model_type: Optional[str] = 'general',
             verbose: Optional[bool] = False,
             **kwargs: Any):
 
@@ -67,8 +67,9 @@ class DawidSkene(BaseLabelModel):
 
         # L = check_weak_labels(dataset_train)
         # n_class = n_class or L.max() + 1
-        L = dataset_train[0]
-        self.n_class = n_class
+        L = dataset_train[0].astype(int)
+        self.n_class = int(L.max() + 1)
+        self.model_type = model_type
 
         Y_p = self._initialize_Y_p(L)
         L_aug = self._initialize_L_aug(L)
@@ -83,7 +84,7 @@ class DawidSkene(BaseLabelModel):
             # M-step
             (class_marginals, error_rates) = self._m_step(L_aug, Y_p)
 
-            # E-setp
+            # E-step
             Y_p = self._e_step(L_aug, class_marginals, error_rates)
 
             # # check likelihood
@@ -107,15 +108,16 @@ class DawidSkene(BaseLabelModel):
         n_class = self.n_class
         n, m = L.shape
 
-        Y_p = np.zeros((n, n_class))
-        initialize_Y_p(Y_p, L, n_class)
+        Y_p = np.zeros((n, self.n_class))
+        initialize_Y_p(Y_p, L, self.n_class)
 
         Y_p /= Y_p.sum(axis=1, keepdims=True)
         return Y_p
 
     def _initialize_L_aug(self, L):
-        L_off = L + 1
-        L_aug = (np.arange(self.n_class + 1) == L_off[..., None]).astype(int)
+        # do not count abstentions as another class
+        L_off = L
+        L_aug = (np.arange(self.n_class) == L_off[..., None]).astype(int)
         return L_aug
 
     def _m_step(self, L_aug, Y_p):
@@ -132,7 +134,31 @@ class DawidSkene(BaseLabelModel):
 
         # # normalize by summing over all observation classes
         s = np.sum(error_rates, axis=-1, keepdims=True)
-        error_rates = np.divide(error_rates, s, where=s != 0)
+
+        if self.model_type == 'one_coin':
+            # want overall accuracy for the one-coin model
+            # we will always assume that there is at least 1 prediction
+            # on the training points
+            for i in range(m):
+                acc = error_rates[i].trace() / s[i].sum()
+                error_rates[i] = (1 - acc) / (self.n_class - 1)
+                np.fill_diagonal(error_rates[i], acc)
+        else:
+            error_rates = np.divide(error_rates, s, where=s != 0)
+
+            if self.model_type == 'class_conditional':
+                for i in range(m):
+                    for j in range(self.n_class):
+                        if s[i, j] == 0:
+                            # if the whole row of the confusion matrix is 0
+                            # set it to the uniform disribution, which will
+                            # not change the answer
+                            error_rates[i, j, :] = 1 / self.n_class
+                        else:
+                            class_acc = error_rates[i, j, j]
+                            error_rates[i, j, :] = (1 - class_acc) / (self.n_class - 1)
+                            error_rates[i, j, j] = class_acc
+
         return (class_marginals, error_rates)
 
     def _e_step(self, L_aug, class_marginals, error_rates):
@@ -143,10 +169,6 @@ class DawidSkene(BaseLabelModel):
         help_e_step(Y_p, error_rates, L_aug)
 
         # normalize error rates by dividing by the sum over all observation classes
-        
-        # use softmax
-        row_maxs = np.amax(s, axis=1)
-        Y_p = np.exp(Y_p - row_maxs)
         s = np.sum(Y_p, axis=-1, keepdims=True)
         Y_p = np.divide(Y_p, s, where=s != 0)
         return Y_p
@@ -173,8 +195,21 @@ class DawidSkene(BaseLabelModel):
 
         return log_L
 
-    def predict_proba(self, dataset: Union[BaseDataset, np.ndarray], **kwargs: Any) -> np.ndarray:
-        L = check_weak_labels(dataset)
+    def predict_proba(self, dataset: Union[BaseDataset, np.ndarray], oracle=False, **kwargs: Any) -> np.ndarray:
+        #L = check_weak_labels(dataset)
+        L = dataset[0].astype(int)
+        self.n_class = int(L.max() + 1)
         L_aug = self._initialize_L_aug(L)
-        Y_p = self._e_step(L_aug, self.class_marginals, self.error_rates)
-        return Y_p
+
+        if oracle:
+            y = dataset[1]
+            # initialize_Y_p does majority vote on the unlabeled data!
+            Y_p = self._initialize_L_aug(dataset[1]).squeeze().astype(float)
+            # Y_p = np.round(self._initialize_Y_p(L))
+            (class_marginals, error_rates) = self._m_step(L_aug, Y_p)
+            Y_p = self._e_step(L_aug, class_marginals, error_rates)
+
+        else:
+            Y_p = self._e_step(L_aug, self.class_marginals, self.error_rates)
+
+        return np.clip(Y_p, 0, 1)
